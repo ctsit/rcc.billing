43df8031dfa95ef9a1e4309679d85229cbd4b99f
Philip Chase <pbc@ufl.edu>
2022-07-20 15:00:56 -0400

    Add get_processed_payment_data_from_email() and shell of receive_payments.R

---
+++ R/read_email.R
@@ -0,0 +1,91 @@
+#' Scrape an inbox for processed payment emails and return data
+#'
+#' Connect to an imap mailbox, identify processed payment emails sent
+#' after `messages_since_date`, and extract the data from those emails.
+#'
+#' @param url The IMAP URL of the host that houses the mailbox
+#' @param username The username of the IMAP mailbox
+#' @param password The password of the IMAP mailbox
+#' @param messages_since_date The sent date of the oldest message that should be inspected
+#'
+#' @return A dataframe of processed payment facts
+#' ##\itemize{
+#' ##  \item email - stuff and things
+#' ##}
+#' @export
+#' @importFrom magrittr "%>%"
+#' @importFrom rlang .data
+#'
+#' @examples
+#' \dontrun{
+#' get_processed_payment_data_from_email(
+#'   username = "jdoe",
+#'   password = "jane_does_password",
+#'   url ="imaps://outlook.office365.com",
+#'   messages_since_date = as.Date("2022-01-01", format = "%Y-%m-%d")
+#'   )
+#' }
+get_processed_payment_data_from_email <- function(username,
+                                                  password,
+                                                  url = "imaps://outlook.office365.com",
+                                                  messages_since_date) {
+  imap_con <- mRpostman::configure_imap(
+    url = url,
+    username = username,
+    password = password
+  )
+
+  imap_con$select_folder("INBOX")
+  emails_by_subject_search <- imap_con$search_string(expr = "Your payment for CTSI Study", where = "SUBJECT")
+  messages_since_date <- imap_con$search_since(date_char = format(messages_since_date, format = "%d-%b-%Y"))
+  emails_found <- dplyr::intersect(emails_by_subject_search, messages_since_date)
+
+  patterns <- c(
+    "Study Name:.*\r\n",
+    "CRC #:.*\r\n",
+    "IDS #:.*\r\n",
+    "OCR #:.*\r\n",
+    "Invoice Number:.*\r\n",
+    "Amount Paid:.*\r\n",
+    "JE #:.*\r\n",
+    "JE Posting Date:.*\r\n"
+  )
+
+  data_from_emails <- tibble::tribble(
+    ~study_name, ~crc_number, ~ids_number, ~ocr_number, ~invoice_number, ~amount_paid, ~je_number, ~je_posting_date, ~ctsi_study_id,
+    "a", "a", "a", "a", "a", "a", "a", "a", "a"
+  ) %>% dplyr::filter(F)
+
+  if (length(emails_found) > 0) {
+    for (email in emails_found) {
+      ctsi_study_id <- email %>%
+        imap_con$fetch_header() %>%
+        stringr::str_extract_all("Subject.*:.*\r\n") %>%
+        sub(".*CTSI Study ", "", .) %>%
+        sub(" has been processed.*", "", .)
+
+      data_row <- email %>%
+        imap_con$fetch_text() %>%
+        stringr::str_extract_all(patterns) %>%
+        # remove html encoded < and > characters
+        sub("&lt;.*&gt;", "", .) %>%
+        # remove literal < and > characters
+        sub("<.*>", "", .) %>%
+        sub("\r\n", "", .) %>%
+        sub("#", "number", .) %>%
+        stringr::str_split(": ") %>%
+        as.data.frame() %>%
+        t() %>%
+        as.data.frame() %>%
+        tidyr::pivot_wider(
+          names_from = "V1",
+          values_from = "V2"
+        ) %>%
+        janitor::clean_names() %>%
+        dplyr::mutate(ctsi_study_id = ctsi_study_id)
+      data_from_emails <- dplyr::bind_rows(data_from_emails, data_row)
+    }
+  }
+
+  return(data_from_emails)
+}
---
+++ etl/receive_payments.R
@@ -0,0 +1,26 @@
+library(rcc.billing)
+library(redcapcustodian)
+library(RMariaDB)
+library(DBI)
+library(tidyverse)
+library(lubridate)
+library(dotenv)
+
+init_etl("receive_payments")
+
+rcc_billing_conn <- connect_to_rcc_billing_db()
+
+earliest_date <- now(tzone = "America/New_York") - ddays(2)
+earliest_date <- ymd("2022-07-06")
+processed_payment_data <- get_processed_payment_data_from_email(
+  username = Sys.getenv("IMAP_USERNAME"),
+  password = Sys.getenv("IMAP_PASSWORD"),
+  messages_since_date = earliest_date
+)
+
+# These lines are useful for testing get_processed_payment_data_from_email()
+# username = Sys.getenv("IMAP_USERNAME")
+# password = Sys.getenv("IMAP_PASSWORD")
+# url = Sys.getenv("IMAP_URL")
+# earliest_date <- ymd("2022-07-06")
+# messages_since_date = earliest_date
