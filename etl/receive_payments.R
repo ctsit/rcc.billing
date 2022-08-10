783784ae652e3222f0f830ad7d43d5e0a7d150ec
Philip Chase <pbc@ufl.edu>
2022-08-10 16:11:19 -0400

    Write processed_payment_data to invoice_line_item_communications

--- etl/receive_payments.R
+++ etl/receive_payments.R
@@ -28,3 +28,38 @@ processed_payment_data_from_email <- get_processed_payment_data_from_email(
 processed_payment_data <- clean_processed_payment_data_from_email(
   processed_payment_data_from_email
 )
+
+max_communications_id <-
+  tbl(rcc_billing_conn, "invoice_line_item_communications") %>%
+  summarise(max_id = max(id, na.rm = TRUE)) %>%
+  collect() %>%
+  pull(max_id)
+
+new_invoice_line_item_communications <- processed_payment_data %>%
+  mutate(
+    id = max_communications_id + row_number(),
+    updated = get_script_run_time(),
+    created = get_script_run_time(),
+    script_name = get_script_name()
+  )
+
+redcapcustodian::write_to_sql_db(
+  conn = rcc_billing_conn,
+  table_name = "invoice_line_item_communications",
+  df_to_write = new_invoice_line_item_communications,
+  schema = NA,
+  overwrite = F,
+  db_name = "rcc_billing",
+  append = T
+)
+
+# TODO Add code to update the invoice_line_item table.
+#      At the moment we are missing a map from the CTSI Study ID to CTSIT's service identifier.
+#      That omission makes this task hacky at best, impossible at worst.
+# new_invoice_line_updates <- processed_payment_data %>%
+#   mutate(
+#     updated = get_script_run_time()
+#   )
+#
+# tbl(rcc_billing_conn, "invoice_line_item") %>%
+#   filter()
